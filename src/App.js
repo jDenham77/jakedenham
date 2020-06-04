bf7b66d928dc5a8fc4da3c1e05da89bc4ef660ea
Jacob Denham <58566596+jDenham77@users.noreply.github.com>
2020-06-04 12:53:31 -0700

    merging

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div>
         <Route path="/" component={Home} exact={true}></Route>
         <Route path="/skills" component={Skills} exact={true}></Route>
-        <Route path="/jakedenham/resume" component={Resume} exact={true}></Route>
+        <Route path="/resume" component={Resume} exact={true}></Route>
         <Route path="/contact" component={ContactMe} exact={true}></Route>
         <Route path="/about" component={AboutMe} exact={true}></Route>
       </div>
